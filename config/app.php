e0f82c84eb3f776b670a0fd3baa76fad43673a02
husam <husamalmiyah@gmail.com>
2023-11-09 14:21:41 +0300

    installing bugsnag

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Bugsnag' => Bugsnag\BugsnagLaravel\Facades\Bugsnag::class,
     ])->toArray(),
 
 ];
